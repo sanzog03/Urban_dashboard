3a2db1094e47f9ff9367292c9443b52b28677f10
Sanjog Thapa <sanzog03@gmail.com>
2024-07-11 16:59:47 -0500

    added title component

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { MapBoxViewer } from '../mapboxViewer';
+import { Title } from '../title';
 
 function Dashboard() {
   return (
@@ -9,6 +10,7 @@ function Dashboard() {
       flexDirection={"column"}
       className="fullSize">
         <MapBoxViewer/>
+        <Title/>
     </Box>
   );
 }
---
+++ src/components/title/index.jsx
@@ -0,0 +1,7 @@
+export function Title () {
+    return (
+        <div id="title">
+            <strong>Urban Dashboard</strong>
+        </div>
+    );
+}
