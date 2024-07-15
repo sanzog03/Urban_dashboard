cf0691a55ae75731658f60de59299734cb446058
Sanjog Thapa <sanzog03@gmail.com>
2024-07-15 15:41:31 -0500

    skeleton for explroe data component with react router

---
+++ src/components/exploreData/index.jsx
@@ -0,0 +1,7 @@
+export function ExploreData() {
+    return (
+        <div className="exploreData">
+            <h1>Explore Data</h1>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import { ExploreData } from './components/exploreData';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/explore",
+    element: <ExploreData />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
